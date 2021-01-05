2fd894cb5cd292b264162dfe5410e66d2a85a358
Matthew Kayode <matthewkayode116@gmail.com>
2021-01-05 13:15:26 +0100

    initial commit

--- src/styles/GalleryWrapper.tsx
+++ src/styles/GalleryWrapper.tsx
@@ -1,8 +1,19 @@
 import styled from 'styled-components'
+import { breakpoints, margins } from './defaults/variables'
 
 export default styled.div`
   display: flex;
   width: 100%;
   overflow-x: scroll;
   grid-column: 1 / span2;
+
+  /* @media screen and (max-width: ${breakpoints.sm}px) {
+    width: calc(100% + ${margins.large}px);
+    margin-left: calc(-${margins.large}px / 2);
+  }
+
+  @media screen and (max-width: ${breakpoints.xs}px) {
+    width: calc(100% + ${margins.small}px);
+    margin-left: calc(-${margins.small}px / 2);
+  } */
 `
